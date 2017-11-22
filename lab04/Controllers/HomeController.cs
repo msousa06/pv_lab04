fa2a481267d40b6a12aa99667e5cf86140f95c52
msousa06 <msousa06@users.noreply.github.com>
2017-11-22 12:27:16 +0000

    nivel 2 concluido

---
+++ lab04/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab04.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Bem vindos";
+            return View();
+        }
+    }
+}
